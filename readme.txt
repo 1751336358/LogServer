e61694bb93984f3c8e7745c67189d4c46d105f2b
liuli <1751336358@qq.com>
2018-04-07 21:27:51 +0800

    2018-04-07

---
+++ readme.txt
@@ -0,0 +1,5 @@
+数据库名systemlog
+数据库表名systemlog_yyyyMMdd
+由于表按天水平分表，所以这里只备份了一张表，其他表结构一样。
+项目可以多节点部署，需要启动Nginx做负载均衡
+Nginx的配置请详见https://github.com/1751336358/kehu51中的配置
--- src/main/java/com/stx/config/MyConfiguration.java
+++ src/main/java/com/stx/config/MyConfiguration.java
@@ -4,10 +4,15 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.stx.pojo.SystemLog;
 
 /**
@@ -32,4 +37,17 @@ public class MyConfiguration {
 		return new RestTemplate();
 	}
 	
+	/**
+	 * 使用FastJson的支持
+	 * @return
+	 */
+	@Bean
+	public HttpMessageConverters fastJsonHttpMessageConverters(){
+		FastJsonHttpMessageConverter fastConverter  = new FastJsonHttpMessageConverter();
+		FastJsonConfig fastJsonConfig = new FastJsonConfig();
+		fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+		fastConverter.setFastJsonConfig(fastJsonConfig);
+		HttpMessageConverter<?> converter = fastConverter;
+		return new HttpMessageConverters(converter);
+	}
 }
